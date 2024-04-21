f9937e712e9d17a449b652cbd9132d66f62bc428
Mateusz Kadula <mateusz.kadula@icloud.com>
2024-04-21 22:13:47 +0200

    restructured

--- backend/app/watch/log.py
+++ backend/app/watch/log.py
@@ -4,7 +4,7 @@ from typing import Any, Self
 import datetime as dt
 from math import sqrt
 
-from app.watch.interpolation.base import InterpolationAbstract
+from backend.app.watch.interpolation.base import InterpolationAbstract
 
 
 class Record:
