19b37e8310114604cb0af1d3486ebb84f47f5fae
Mateusz Kadula <mateusz.kadula@icloud.com>
2024-05-13 18:57:29 +0200

    minor changes

--- backend/settings.py
+++ backend/settings.py
@@ -1,8 +1,7 @@
 from pathlib import Path
-import os
 
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 8001
 
-BASE_DIR = Path(__file__).parent.parent  # os.getenv('BASE_DIR')
+BASE_DIR = Path(__file__).parent.parent
 DATABASE_PATH = BASE_DIR / 'watchDB.sqlite3'
